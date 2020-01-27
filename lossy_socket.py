0433b5d6d2937d7973c3fe93c7b0dacb51efb0cb
Steve Tarzia <steve@stevetarzia.com>
2020-01-27 15:25:49 -0600

    store LossyUDP stats gobally, for testing observability

--- lossy_socket.py
+++ lossy_socket.py
@@ -1,6 +1,6 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 import random
-from threading import Timer
+from threading import Timer, Lock
 from time import sleep
 from typing import Tuple
 
@@ -15,34 +15,42 @@ class SimulationParams:
         self.corruption_rate = corruption_rate
         self.max_delivery_delay = max_delivery_delay
 
-# global simulation parameters
-sim = SimulationParams()
-
-
-class LossyUDP(socket):
+class SimulationStats:
     def __init__(self):
         self.packets_sent = 0
         self.packets_recv = 0
         self.bytes_sent = 0
         self.bytes_recv = 0
-        super().__init__(AF_INET, SOCK_DGRAM)
+        self.lock = Lock()
 
     def __del__(self):
-        # for our purposes, we always want to unbind the port when the app stops
-        super().close()
         # print some stats
         print("PACKETS_SENT=%d" % self.packets_sent)
         print("PACKETS_RECV=%d" % self.packets_recv)
         print("BYTES_SENT=%d" % self.bytes_sent)
         print("BYTES_RECV=%d" % self.bytes_recv)
 
+
+# global simulation parameters
+sim = SimulationParams()
+stats = SimulationStats()
+
+class LossyUDP(socket):
+    def __init__(self):
+        super().__init__(AF_INET, SOCK_DGRAM)
+
+    def __del__(self):
+        # for our purposes, we always want to unbind the port when the app stops
+        super().close()
+
     def sendto(self, message: bytes, dst: Tuple[str, int]):
         """Unlike the sendto method provided by the BSD socket lib,
            this method never blocks (because it schedules the transmission on a thread)."""
         if len(message) > 1472:
             raise RuntimeError("You are trying to send more than 1472 bytes in one UDP packet!")
-        self.packets_sent += 1
-        self.bytes_sent += len(message)
+        with stats.lock:
+            stats.packets_sent += 1
+            stats.bytes_sent += len(message)
         # sleep() spaces out the requests enough to eliminate reordering caused by the OS process/thread scheduler.
         # It also limits the peak throughput of the socket :(
         sleep(0.01)
@@ -77,8 +85,9 @@ class LossyUDP(socket):
         while True:
             try:
                 data, addr = super().recvfrom(bufsize)
-                self.packets_recv += 1
-                self.bytes_recv += len(data)
+                with stats.lock:
+                    stats.packets_recv += 1
+                    stats.bytes_recv += len(data)
             except InterruptedError:
                 # note that on Python >= 3.5, this exception will not happen:
                 # https://www.python.org/dev/peps/pep-0475/
